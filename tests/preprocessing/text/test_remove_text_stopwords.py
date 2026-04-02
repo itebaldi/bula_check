3d3e6041cb7eb0de00d551f223d130a720dc9603
itebaldi <isadoratebaldi@gmail.com>
2026-04-02 17:32:59 -0300

    test: fix test_remove_text_stopwords file

--- tests/preprocessing/text/test_remove_text_stopwords.py
+++ tests/preprocessing/text/test_remove_text_stopwords.py
@@ -1,24 +1,24 @@
 from bula_check.preprocessing.text import remove_text_stopwords
+from inputs.stopwords import get_portuguese_stopwords
+
+STOP_WORDS = get_portuguese_stopwords()
 
 
-def test_remove_text_stopwords_from_text():
-    stop_words = {"a", "o", "que", "de"}
+def test_remove_text_stopwords():
     text = "o rato roeu a roupa do rei de roma"
     expected = "rato roeu roupa rei roma"
-    remover = remove_text_stopwords(stop_words=stop_words)
+    remover = remove_text_stopwords(stop_words=STOP_WORDS)
     assert remover(text) == expected
 
 
-def test_remove_text_stopwords_is_case_insensitive():
-    stop_words = {"UM", "Com"}
+def test_remove_text_stopwords__is_case_insensitive():
     text = "Um teste com palavras"
     expected = "teste palavras"
-    remover = remove_text_stopwords(stop_words=stop_words)
+    remover = remove_text_stopwords(stop_words=STOP_WORDS)
     assert remover(text) == expected
 
 
-def test_remove_text_stopwords_with_no_stopwords():
-    stop_words = {"x", "y", "z"}
+def test_remove_text_stopwords__with_no_stopwords():
     text = "nenhuma stopword aqui"
-    remover = remove_text_stopwords(stop_words=stop_words)
+    remover = remove_text_stopwords(stop_words=STOP_WORDS)
     assert remover(text) == text
