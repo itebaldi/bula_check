8b5016a927ff711df8fd160b19ab193b6270a0c0
itebaldi <isadoratebaldi@gmail.com>
2026-04-04 19:24:06 -0300

    feat: add constants file

--- bula_check/constants.py
+++ bula_check/constants.py
@@ -18,3 +18,24 @@ LANGUAGES = Literal[
     "spanish",
     "swedish",
 ]
+SECTION_PATTERNS: dict[str, list[str]] = {
+    "indications": [
+        "para que este medicamento e indicado",
+        "indications",
+    ],
+    "contraindications": [
+        "quando nao devo usar este medicamento",
+        "contraindications",
+        "quem nao deve usar este medicamento",
+    ],
+    "warnings_and_precautions": [
+        "o que devo saber antes de usar este medicamento",
+        "warnings_and_precautions",
+        "precaucoes",
+    ],
+    "adverse_reactions": [
+        "quais os males que este medicamento pode me causar",
+        "reacoes adversas",
+        "efeitos adversos",
+    ],
+}
